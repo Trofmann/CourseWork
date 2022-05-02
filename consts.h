0536d29af44301d3e7b08b8c40675d15303bae21
Evgeniy <trofimovevgeniy9@gmail.com>
2022-05-02 18:13:33 +0300

    Знаки

--- consts.h
+++ consts.h
@@ -12,4 +12,7 @@ extern const vector<string> keyWords;
 // Список всех разделителей
 extern const vector<string> spacers;
 
+// Список всех знаков
+extern const vector<string> signs;
+
 #endif //COURSEWORK_CONSTS_H
