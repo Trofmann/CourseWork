d7525d33de05b290d97e201f7aa2da7b33fef868
Evgeniy <trofimovevgeniy9@gmail.com>
2022-05-02 16:54:52 +0300

    Подготовка к выносу констант в отдельный файл

---
+++ consts.cpp
@@ -0,0 +1,12 @@
+#include <string>
+#include <vector>
+#include "consts.h"
+
+using namespace std;
+
+// Списки значений для постоянных групп
+// Список всех ключевых слов.
+vector<string> keyWords = {
+        "alignas",
+        "alignof"
+};
---
+++ consts.h
@@ -0,0 +1,12 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+#ifndef COURSEWORK_CONSTS_H
+#define COURSEWORK_CONSTS_H
+
+// Список всех ключевых слов.
+extern vector<string> keyWords;
+
+#endif //COURSEWORK_CONSTS_H
